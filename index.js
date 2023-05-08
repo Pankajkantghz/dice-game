6c56a20afcb179e8b2b0b5f4084ef79a85915074
Pankajkantghz <118116376+Pankajkantghz@users.noreply.github.com>
2023-05-08 14:58:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;//creating 1-6 random numbers
-var randomImage1="images/dice"+randomNumber1+".png";//1-6 random images 
+var randomImage1="dice"+randomNumber1+".png";//1-6 random images 
 document.querySelectorAll("img")[0].setAttribute("src",randomImage1);//make chage at src 
   
 var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomImage2="images/dice"+randomNumber2+".png";
+var randomImage2="dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
 //based upon above winner is decided by following below relations
 if(randomNumber1>randomNumber2){
